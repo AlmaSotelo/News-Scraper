80001f3a61680f15b673bcbd77bc5e5a775bf9fe
Alma@github.noreply.github.com <almatcss@yahoo.com>
2019-01-19 04:02:22 -0700

    first push

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,123 @@
+/* global bootbox */
+$(document).ready(function() {
+   //Setting a reference to the article-container div where all the dynamic content will fo
+   //Adding event listeners to any dynamically generated "save article"
+   //and "scrape new article" buttons
+   var articleContainer = $(".article-container");
+   $(document).on("click", "btn.save", handleArticleSave);
+   $(document).on("click", ".scrape-new", handleArticleScrape);
+
+   //once the page is ready, run the initPage function to cick things off
+   initPage();
+
+   function initPage () {
+      //Empty the article container, run an AJAX request for any usaved headlines
+      articleContainer.empty();
+      $.get("/api/headlines?saved=false")
+        .then(function(data) {
+           // If we have headlines, render them to the page
+           if (data && data.length) {
+              renderArticles(data);
+           }
+           else {
+              // Otherwise render a message explaining wehave no articles
+              renderEmpty();
+           }
+        });
+   }
+
+   function renderArticles(articles) {
+      // This function handles appending HTML containing our article data to the page
+      //We are passed an array of JSON containing all available articles in our database
+      var articlePanels = [];
+      //We pass each article JSON object to the createPanel function which returns a bootstrap
+      //panel with our article data inside
+      for (var i=0; i< articles.length; i++ ) {
+         afticlePanels.push(createPanel(articles[i]));
+      }
+      //Once we have all of the HTML for the articles stored in our articlePanel array,
+      //append them to the articlePanels container
+      articleContainer.append(articlePanels);
+   }
+
+   function createPanel(article) {
+      //This function takes in a sinfle JSON object for an article/headline
+      //It contructs a jQuery element containing all of the formatted HTML for the
+      //article panel
+      var panel =
+         $(["div class='panel panel-default'>",
+            "div class='panel-heading'>", 
+            "<h3>",
+            article.headline,
+            "<a class='btn-success save'>",
+            "Save Article",
+            "</a>",
+            "</h3>",
+            "</div",
+            "<div class='panel-body'>",
+            article.summary,
+            "</div>",
+            "</div>"
+         ].join(""));
+         //We attach the article's id to the jQuery element
+         //We will use this when trying to figure out which article the user wants to save
+      panel.data("_id", article._id);
+      //We return the constructed panel jQuery element
+      return panel;
+   }
+
+   function renderEmpty() {
+      //This function render some HTML to the page explaining we don't have any articles to view
+      //Using a joined array of HTML string data because it's easier to read/change than a concatenated string
+      var emptyAlert =
+         $(["<div class='alert alert-warning text-center'>",
+         "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
+         "<div>",
+         "<div class='panel panel-default'>",
+         "<div class='panel-heading text-center'>",
+         "<h3>What Would You Like to Do?<h3>",
+         "</div>",
+         "<div class='panel-body text-center'>",
+         "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
+         "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
+         "</div>",
+         "</div>",
+         ].join(""));
+      //Appending this data to the page
+      articleContainer.append(emptyAlert);
+   }
+
+   function handleArticleSave () {
+      //This function is triggered when the user wants to save an article
+      //When we rendered the article initially, we attatched a javascript object containing the headline id
+      //to the elementusing the .data method. Here we retrieve that.
+      var articleToSave = $(this).parents(".panel").data();
+      articleToSave.saved = true;
+      //Using a patch method to be semantic since this is an update to an existing record in our collection
+      $.ajax({
+         method: "PATCH",
+         url: "/api/headlines",
+         data: articleToSave
+      })
+      . then(function(data) {
+         // If successful, monfoose will send back an object containing a key of "ok" with the value of 1
+         //(which casts to 'true'
+         if (data.ok) {
+            //Run the initPage function again. This will reload the entire list of articles
+            initPage();
+         }
+      });
+   }
+
+   function handleArticleScrape() {
+      //This function handles the user clicking any "scrape new article" buttons
+      $.get("/api/fetch")
+        .then(function(data) {
+            //if we are able to succesfully scrape the NYTIMES and compare the articles to those
+            //already in our collectiom re render the article on the page
+            //and let the user know how many unique articles we were able to save
+            initPage();
+            bootbox.alert("<h3 class='text-center m-top-80'>" + data.message + "</h3>");
+        });
+   }
+});
---
+++ server.js
@@ -0,0 +1,53 @@
+//Require out dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");//This was added after we check the server was actually listening
+var bodyParser = require("body-parser");//This was added after we check the server was actually listening
+
+//Set up our port to be either the host's designated port, or 3000
+var PORT = process.env.PORT || 3000;
+
+//Instantiate our Express App
+var app = express();
+
+//Set up an Express Router
+var router = express.Router();
+
+//Require our routes file pass our router object
+require("./config/routes")(router);
+
+//Designate our public folder as a static directory
+app.use(express.static(__dirname + "/public"));
+
+//Connect Handlebars to our Exoress app. This was added after we check the server was actually listening
+app.engine("handlebars", expressHandlebars ({
+   defaultLayout: "main"
+}));
+app.set("view engine", "handlebars"); 
+
+//Use bodyParser in our app. This was added after we check the server was actually listening
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
+//Have every request go trhough our router middleware
+app.use(router);
+
+//If deployed, use the deployed database. Otherwise use the local mongoHeadLines database
+var db = process.env.MONGODB_URL || "mongodb://localhost/mongoHeadLines";
+
+//Connect mongoose to our database
+mongoose.connect(db, function(error) {
+   //Log any errors connecting with mongoose
+   if (error) {
+      console.log(error);
+   }
+   //Or log a success message
+   else {
+      console.log("mongoose connection is successful");
+   }
+})
+//Listen on the port
+app.listen(PORT, function() {
+   console.log("Listening on port:" + PORT);
+});
